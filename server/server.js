c6a9c64757df652b6bf9c59870e772f89f67a7fb
luis.hernandez <190388@utags.edu.mx>
2020-12-03 03:08:21 -0600

    Conexión lista a BD

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config')
 const express = require('express')
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 const app = express()
 
@@ -59,6 +60,16 @@ app.get('/usuario', function (req, res) {
         id: id,
     })
   })
+
+  mongoose.connect('mongodb://localhost:27017/cafeteria', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }, (err, res) => {
+  if (err) throw err;
+  console.log('Base de datos ONLINE')
+});
 
 app.listen(process.env.PORT, () => {
     console.log('El servidor esta en linea por el puerto: ', process.env.PORT);
