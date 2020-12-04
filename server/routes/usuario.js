12735fb9a7667c7199372b4aa04cdbec00b2ddc1
luis.hernandez <190388@utags.edu.mx>
2020-12-04 02:53:00 -0600

    Realizado el GET y PUT de la ruta Usuario

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,13 +1,33 @@
 const express = require('express')
+const _ = require('underscore')
 const Usuario = require('../models/usuario')
 const app = express()
 
   app.get('/usuario', function (req, res) {
-      res.json({
-          ok: '200',
-          mensaje: 'usuarios consultados con exito'
+    let desde = req.query.desde || 0
+    let hasta = req.query.hasta || 5
+
+      Usuario.find({ estado: true })
+      .skip(Number(desde))
+      .limit(Number(hasta))
+      .exec((err, usuarios) => {
+        if(err) {
+          return res.status(400).json({
+            ok: false,
+            msg: 'Ocurrio un error al momento de consultar',
+            err
+          })
+        }
+
+        res.json({
+          ok: true,
+          msj: 'Lista de usuarios obtenida con exito',
+          conteo: usuarios.length,
+          usuarios
+        })
       })
     })
+    
   
     app.post('/usuario', function (req, res) {
       let body = req.body
@@ -34,16 +54,27 @@ const app = express()
       })
     })
   
-    app.put('/usuario/:id/:nombre', function (req, res) {
+    app.put('/usuario/:id', function (req, res) {
       let id = req.params.id
-      let nombre = req.params.nombre
-  
-      res.json({
-          ok: 200,
-          mensaje: 'usuario actualizado con exito',
-          id: id,
-          nombre: nombre
-      })
+      let body = _.pick(req.body, ['nombre', 'email'])
+
+      Usuario.findByIdAndUpdate(id, body, 
+        { new:true, runValidators: true, context: 'query' }, 
+        (err, usrBD) => {
+          if(err) {
+            return res.status(400).json({
+              ok: true,
+              msj: 'Ocurrio un error al momento de actualizar',
+              err
+            })
+          }
+
+          res.json({
+            ok:true,
+            msj: 'Usuario actualizo con exito',
+            usuario: usrBD
+          })
+        })
     })
   
     app.delete('/usuario/:id', function (req, res) {
